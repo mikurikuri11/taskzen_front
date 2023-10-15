f9e0c5ee6fdda4fc9be4f6c16a0928ac61a570f5
mikurikuri1919 <mikuriyakenta003@gmail.com>
2023-10-15 17:03:41 +0900

    ✨feat：create header and footer component #4

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,7 @@
-"use client";
-
-import { getHello, Hello } from "@/helloAPI"
-
 export default function Home() {
-  const toggleButton = async () => {
-    const hello: Hello = await getHello();
-    console.log(hello);
-  }
   return (
     <>
       <h1>Home</h1>
-      <button
-        onClick={() => toggleButton()}
-        className="bg-green-400 font-medium p-2"
-      >
-          Hello
-      </button>
     </>
   )
 }
