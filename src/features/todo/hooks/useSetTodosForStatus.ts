7e629d5671b94d306a016138d710f723ac4eedcb
mikurikuri1919 <mikuriyakenta003@gmail.com>
2023-11-09 09:49:00 +0900

    🧼clean：refactor TodoManagement.tsx #26

---
+++ src/features/todo/hooks/useSetTodosForStatus.ts
@@ -0,0 +1,23 @@
+import { useEffect } from 'react'
+import { useSetRecoilState } from 'recoil'
+import { Todo } from '@/features/todo/api/types/index'
+import { completedTodoAtom } from '@/recoil/atoms/completedTodoAtom'
+import { incompletedTodoAtom } from '@/recoil/atoms/incompletedTodoAtom'
+
+export function useSetTodosForStatus(todos: Todo[]) {
+  const setCompletedTodos = useSetRecoilState(completedTodoAtom)
+  const setIncompletedTodos = useSetRecoilState(incompletedTodoAtom)
+
+  useEffect(() => {
+    const incompletedTodos = todos.filter((todo) => !todo.completed)
+    setIncompletedTodos(incompletedTodos)
+
+    const completedTodos = todos.filter((todo) => todo.completed)
+    setCompletedTodos(completedTodos)
+  }, [todos])
+
+  return {
+    setCompletedTodos,
+    setIncompletedTodos,
+  }
+}
