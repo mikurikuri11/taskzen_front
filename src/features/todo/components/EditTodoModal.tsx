439db0b5cd800454f05da07527bd5b9bd0be865d
mikurikuri1919 <mikuriyakenta003@gmail.com>
2023-12-04 23:21:34 +0900

    ✨feat：add feature for updating todoCategories #56

--- src/features/todo/components/EditTodoModal.tsx
+++ src/features/todo/components/EditTodoModal.tsx
@@ -3,18 +3,19 @@
 import { Dialog, Transition } from '@headlessui/react'
 
 import { useSession } from 'next-auth/react'
-import { useState, useEffect, FC, Fragment } from 'react'
+import { useEffect, FC, Fragment, useState } from 'react'
 import { useForm, SubmitHandler, set } from 'react-hook-form'
 import { useSetRecoilState } from 'recoil'
 import { deleteTodo } from '../api/deleteTodo'
 import { editTodo } from '../api/editTodo'
 import { Todo } from '../api/types'
 import { StyledSubmitButton } from '@/components/ui/Button/StyledSubmitButton'
-import { deleteCategory } from '@/features/category/api/deleteCategory'
-import { getTodoCategories } from '@/features/category/api/getTodoCategories'
+import { deleteCategory } from '@/features/category/api/category/deleteCategory'
+import { getTodoCategories } from '@/features/category/api/todoCategory/getTodoCategories'
 import { Category } from '@/features/category/api/types'
 import { CategoryFlyoutMenu } from '@/features/category/components/CategoryFlyoutMenu'
 import { getTodos } from '@/features/todo/api/getTodos'
+import { ModalTodoAtom } from '@/recoil/atoms/modalTodoAtom'
 import { TodoAtom } from '@/recoil/atoms/todoAtom'
 
 type Props = {
@@ -34,7 +35,14 @@ export const EditTodoModal: FC<Props> = (props) => {
 
   const { data: session, status } = useSession()
   const setTodos = useSetRecoilState(TodoAtom)
-  const [todoCategories, setTodoCategories] = useState<Category[]>();
+  const [todoCategories, setTodoCategories] = useState<Category[] | null>(null)
+  const setModalTodo = useSetRecoilState(ModalTodoAtom);
+
+  useEffect(() => {
+    if (todo) {
+      setModalTodo(todo)
+    }
+  }, [todo]);
 
   const onSubmit: SubmitHandler<Todo> = async (data) => {
     if (todo?.id) {
@@ -43,19 +51,10 @@ export const EditTodoModal: FC<Props> = (props) => {
       if (updatedTodos) {
         setTodos(updatedTodos)
       } else {
-        setTodos([]);
+        setTodos([])
       }
       setOpen(false)
       reset()
-    }
-  }
-
-  const fetchTodoCategories = async () => {
-    if (todo) {
-      const todoCategories = await getTodoCategories({ id: todo.id })
-      console.log(todoCategories);
-      setTodoCategories(todoCategories)
-      console.log(todoCategories);
     }
   }
 
@@ -63,7 +62,14 @@ export const EditTodoModal: FC<Props> = (props) => {
     await deleteCategory({ id })
 
     if (!todo) {
-      return null;
+      return null
+    }
+  }
+
+  const fetchTodoCategories = async () => {
+    if (todo) {
+      const todoCategories = await getTodoCategories({ id: todo.id })
+      setTodoCategories(todoCategories)
     }
   }
 
@@ -130,32 +136,36 @@ export const EditTodoModal: FC<Props> = (props) => {
                           className='block text-sm font-medium leading-6 text-gray-900'
                         >
                           カテゴリー
-                          {todo?.category_ids}
                         </label>
                         <CategoryFlyoutMenu />
                       </div>
-                      {todoCategories && todoCategories.map((category) => (
-                        <span
-                          key={category.id}
-                          className='ml-1 mt-2 inline-flex items-center rounded-full border border-gray-200 bg-white py-1.5 pl-3 pr-2 text-sm font-medium text-gray-900'
-                        >
-                          <span>{category.name}</span>
-                          <button
-                            onClick={() => category.id && onClickDelete(category.id)}
-                            type='button'
-                            className='inline-flex h-4 w-4 flex-shrink-0 rounded-full p-1 text-gray-400 hover:bg-gray-200 hover:text-gray-500'
+                      {todoCategories &&
+                        todoCategories.map((category) => (
+                          <span
+                            key={category.id}
+                            className='ml-1 mt-2 inline-flex items-center rounded-full border border-gray-200 bg-white py-1.5 pl-3 pr-2 text-sm font-medium text-gray-900'
                           >
-                            <svg
-                              className='h-2 w-2'
-                              stroke='currentColor'
-                              fill='none'
-                              viewBox='0 0 8 8'
+                            <span>{category.name}</span>
+                            <button
+                              onClick={() => category.id && onClickDelete(category.id)}
+                              type='button'
+                              className='inline-flex h-4 w-4 flex-shrink-0 rounded-full p-1 text-gray-400 hover:bg-gray-200 hover:text-gray-500'
                             >
-                              <path strokeLinecap='round' strokeWidth='1.5' d='M1 1l6 6m0-6L1 7' />
-                            </svg>
-                          </button>
-                        </span>
-                      ))}
+                              <svg
+                                className='h-2 w-2'
+                                stroke='currentColor'
+                                fill='none'
+                                viewBox='0 0 8 8'
+                              >
+                                <path
+                                  strokeLinecap='round'
+                                  strokeWidth='1.5'
+                                  d='M1 1l6 6m0-6L1 7'
+                                />
+                              </svg>
+                            </button>
+                          </span>
+                        ))}
                     </div>
 
                     <div className='sm:col-span-4'>
