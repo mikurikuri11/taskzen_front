f37636a8b4d24c448afd0eeb2e92a4cbe319380e
mikurikuri1919 <mikuriyakenta003@gmail.com>
2023-12-18 10:06:56 +0900

    🧼clean：refactor TodoManagement with custom hook #101

--- src/features/todo/components/TodoManagement.tsx
+++ src/features/todo/components/TodoManagement.tsx
@@ -1,39 +1,21 @@
-'use client'
+"use client";
 
-import { useState, useEffect, FC } from 'react'
-import { useRecoilState } from 'recoil'
+import { FC } from 'react'
 import { StyledButton } from '@/components/ui-elements/Button/StyledButton'
 import { Sidebar } from '@/features/todo/components/Sidebar'
 import { TodoMatrix } from '@/features/todo/components/TodoMatrix'
-import { useSetTodosForStatus } from '@/features/todo/hooks/useSetTodosForStatus'
-import { useSetTodos } from '@/hooks/useSetTodos'
-import { incompletedTodoAtom } from '@/recoil/atoms/incompletedTodoAtom'
+import { useTodoManagement } from '@/features/todo/hooks/useTodoManagement'
 
 export const TodoManagement: FC = () => {
-  const [open, setOpen] = useState(false)
-
-  const todos = useSetTodos()
-  useSetTodosForStatus(todos)
-
-  const [incompletedTodos, setIncompletedTodos] = useRecoilState(incompletedTodoAtom)
-
-  const filterTodosByZone = (zone: number) => incompletedTodos.filter((todo) => todo.zone === zone)
-
-  const [todosByOne, setTodosByOne] = useState(filterTodosByZone(1))
-  const [todosByTwo, setTodosByTwo] = useState(filterTodosByZone(2))
-  const [todosByThree, setTodosByThree] = useState(filterTodosByZone(3))
-  const [todosByFour, setTodosByFour] = useState(filterTodosByZone(4))
-
-  useEffect(() => {
-    setTodosByOne(filterTodosByZone(1))
-    setTodosByTwo(filterTodosByZone(2))
-    setTodosByThree(filterTodosByZone(3))
-    setTodosByFour(filterTodosByZone(4))
-  }, [incompletedTodos])
-
-  const openSidebar = () => {
-    setOpen(true)
-  }
+  const {
+    open,
+    setOpen,
+    todosByOne,
+    todosByTwo,
+    todosByThree,
+    todosByFour,
+    openSidebar,
+  } = useTodoManagement()
 
   return (
     <>
---
+++ src/features/todo/hooks/useTodoManagement.ts
@@ -0,0 +1,53 @@
+import { useState, useEffect, FC } from 'react'
+import { useRecoilValue } from 'recoil'
+import { Todo } from '@/features/todo/api/types/index'
+import { useSetTodosForStatus } from '@/features/todo/hooks/useSetTodosForStatus'
+import { useSetTodos } from '@/hooks/useSetTodos'
+import { incompletedTodoAtom } from '@/recoil/atoms/incompletedTodoAtom'
+
+type UseTodoManagementResult = {
+  open: boolean;
+  setOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  todosByOne: Todo[];
+  todosByTwo: Todo[];
+  todosByThree: Todo[];
+  todosByFour: Todo[];
+  openSidebar: () => void;
+};
+
+export const useTodoManagement = (): UseTodoManagementResult => {
+  const [open, setOpen] = useState(false)
+
+  const todos = useSetTodos()
+  useSetTodosForStatus(todos)
+
+  const incompletedTodos = useRecoilValue(incompletedTodoAtom)
+
+  const filterTodosByZone = (zone: number) => incompletedTodos.filter((todo) => todo.zone === zone)
+
+  const [todosByOne, setTodosByOne] = useState(filterTodosByZone(1))
+  const [todosByTwo, setTodosByTwo] = useState(filterTodosByZone(2))
+  const [todosByThree, setTodosByThree] = useState(filterTodosByZone(3))
+  const [todosByFour, setTodosByFour] = useState(filterTodosByZone(4))
+
+  useEffect(() => {
+    setTodosByOne(filterTodosByZone(1))
+    setTodosByTwo(filterTodosByZone(2))
+    setTodosByThree(filterTodosByZone(3))
+    setTodosByFour(filterTodosByZone(4))
+  }, [incompletedTodos])
+
+  const openSidebar = () => {
+    setOpen(true)
+  }
+
+  return {
+    open,
+    setOpen,
+    todosByOne,
+    todosByTwo,
+    todosByThree,
+    todosByFour,
+    openSidebar,
+  }
+}
