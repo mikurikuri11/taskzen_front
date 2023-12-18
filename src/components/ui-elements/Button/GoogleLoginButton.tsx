b4508cf2891a6f1a5ac8977194ed4eb437244613
mikurikuri1919 <mikuriyakenta003@gmail.com>
2023-12-18 22:20:58 +0900

    ✨feat：add react-joyride #101

--- src/components/ui-elements/Button/GoogleLoginButton.tsx
+++ src/components/ui-elements/Button/GoogleLoginButton.tsx
@@ -7,7 +7,7 @@ export const GoogleLoginButton = () => {
     <button
       type='button'
       className='inline-flex w-full justify-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white hover:bg-indigo-500 '
-      onClick={() => signIn('google', {}, { prompt: 'login' })}
+      onClick={() => signIn('google', { callbackUrl: '/todos'}, { prompt: 'login' })}
     >
       Login With Google
     </button>
