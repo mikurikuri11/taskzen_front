b1976d2255dc652dd311334d07a66a6f7557ff24
mikurikuri1919 <mikuriyakenta003@gmail.com>
2023-12-24 20:39:53 +0900

    ✨feat：add splide in top page #105

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,44 +1,99 @@
-'use client'
+"use client";
 
-import Link from 'next/link'
-import { useSession } from 'next-auth/react'
-import { useRecoilState } from 'recoil'
+import {
+  ArrowPathIcon,
+  CloudArrowUpIcon,
+  Cog6ToothIcon,
+  FingerPrintIcon,
+  LockClosedIcon,
+  ServerIcon,
+} from '@heroicons/react/20/solid'
 
-import { LogoutButton } from '@/components/ui-elements/Button/LogoutButton'
-import { StyledButton } from '@/components/ui-elements/Button/StyledButton'
-import { LoginModal } from '@/components/ui-elements/Modal/LoginModal'
-import { showLoginModalAtom } from '@/recoil/atoms/showLoginModalAtom'
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+import '@splidejs/splide/css'; 
+
+const features = [
+  {
+    name: 'Push to deploy.',
+    description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit aute id magna.',
+    icon: CloudArrowUpIcon,
+  },
+  {
+    name: 'SSL certificates.',
+    description: 'Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo.',
+    icon: LockClosedIcon,
+  },
+  {
+    name: 'Simple queues.',
+    description: 'Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus.',
+    icon: ArrowPathIcon,
+  },
+  {
+    name: 'Advanced security.',
+    description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit aute id magna.',
+    icon: FingerPrintIcon,
+  },
+  {
+    name: 'Powerful API.',
+    description: 'Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo.',
+    icon: Cog6ToothIcon,
+  },
+  {
+    name: 'Database backups.',
+    description: 'Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. ',
+    icon: ServerIcon,
+  },
+]
 
 export default function Home() {
-  const [showLoginModal, setShowLoginModal] = useRecoilState(showLoginModalAtom)
-  const { data: session, status } = useSession()
-
-  const openModal = () => {
-    setShowLoginModal(true)
-  }
-
   return (
-    <div className='h-screen flex flex-col justify-center items-center'>
-      <div className='text-2xl font-semibold text-white mb-10'>
-        あなたの日々の生活に７つの習慣を取り入れてみましょう。きっと生活が豊かになります。
+    <>
+    <div className="bg-gray-900 py-24 sm:py-32">
+      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        <div className="mx-auto max-w-2xl sm:text-center">
+          <h2 className="text-base font-semibold leading-7 text-indigo-400">Everything you need</h2>
+          <p className="mt-2 text-3xl font-bold tracking-tight text-white sm:text-4xl">TaskZenn</p>
+          <p className="mt-6 text-lg leading-8 text-gray-300">
+            あなたの日々の生活に７つの習慣を取り入れてみましょう。<br />
+            きっと生活が豊かになります。
+          </p>
+        </div>
       </div>
-      <div className='flex'>
-        {status !== 'authenticated' ? (
-          <StyledButton buttonStyle='bg-indigo-500' onClick={openModal}>
-            ログイン
-          </StyledButton>
-        ) : (
-          <LogoutButton />
-        )}
-        <Link
-          href='/'
-          passHref
-          className='rounded-md px-3 text-lg font-semibold text-white hover:text-gray-300 pt-3 ml-4'
-        >
-          学んでみる
-        </Link>
-        <LoginModal open={showLoginModal} setOpen={setShowLoginModal} />
+      <div className="relative overflow-hidden pt-16">
+        <div className="mx-auto max-w-7xl px-6 lg:px-8">
+          <Splide
+            aria-label="私のお気に入りの画像集"
+            options={{
+              autoplay: true, // 自動再生を有効
+              interval: 3000, // 自動再生の間隔を3秒に設定
+            }}
+          >
+            <SplideSlide>
+              <img className="w-full h-full object-cover" src="https://www.pakutaso.com/shared/img/thumb/shikun20220402_114719-2_TP_V.jpg" alt="かわいい猫の画像 part1" />
+            </SplideSlide>
+            <SplideSlide>
+              <img className="w-full h-full object-cover" src="https://www.pakutaso.com/shared/img/thumb/shikun20220402_122123_TP_V.jpg" alt="かわいい猫の画像 part2" />
+            </SplideSlide>
+            <SplideSlide>
+              <img className="w-full h-full object-cover" src="https://www.pakutaso.com/shared/img/thumb/sikun_20220402-180657-2_TP_V.jpg" alt="かわいい猫の画像 part3" />
+            </SplideSlide>
+          </Splide>
+        </div>
+      </div>
+      <div className="mx-auto mt-16 max-w-7xl px-6 sm:mt-20 md:mt-24 lg:px-8">
+        <dl className="mx-auto grid max-w-2xl grid-cols-1 gap-x-6 gap-y-10 text-base leading-7 text-gray-300 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-3 lg:gap-x-8 lg:gap-y-16">
+          {features.map((feature) => (
+            <div key={feature.name} className="relative pl-9">
+              <dt className="inline font-semibold text-white">
+                <feature.icon className="absolute left-1 top-1 h-5 w-5 text-indigo-500" aria-hidden="true" />
+                {feature.name}
+              </dt>{' '}
+              <dd className="inline">{feature.description}</dd>
+            </div>
+          ))}
+        </dl>
       </div>
     </div>
+    </>
   )
 }
