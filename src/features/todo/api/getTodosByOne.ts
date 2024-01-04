20218e508530d8877f5a41dfb66b086f839f8ed6
mikurikuri1919 <mikuriyakenta003@gmail.com>
2024-01-04 10:19:32 +0900

    🐞fix：add todo api #107

---
+++ src/features/todo/api/getTodosByOne.ts
@@ -0,0 +1,14 @@
+import { Todo } from './types/index'
+
+type Props = {
+  id: string
+}
+
+export const getTodosByOne = async (props: Props): Promise<Todo[]> => {
+  const { id } = props
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/v1/todos/incomplete_todo_by_one/${id}`, {
+    cache: 'no-store',
+  })
+  const todos = await res.json()
+  return todos
+}
