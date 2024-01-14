12019ca0a9ee08a33aa4b727596213d01f9f9779
mikurikuri1919 <mikuriyakenta003@gmail.com>
2024-01-14 15:33:42 +0900

    🐛 マトリックス表のハイライトを変更

--- src/features/todo/components/TodoZone.tsx
+++ src/features/todo/components/TodoZone.tsx
@@ -6,11 +6,15 @@ type TodoZoneProps = {
 };
 
 export const TodoZone = ({ zone, todos }: TodoZoneProps) => (
-  <div className={`group relative bg-${zone === 2 ? 'yellow-100' : 'white'} p-6 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-500 w-96 h-72 border-l ${zone > 1 ? 'divide-y divide-gray-200' : ''}`}>
+  <div className={`group relative bg-white p-6 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-500 w-96 h-72 border-l ${zone > 1 ? 'divide-y divide-gray-200' : ''}`}>
     <div className='mt-1'>
       <h3 className='text-base font-semibold leading-6 text-gray-900'>
         <span className='absolute inset-0' aria-hidden='true' />
-        第{zone}の領域
+        {zone === 2 ? (
+          <span className='bg-blue-200 rounded-lg p-1'>第{zone}の領域</span>
+        ) : (
+          <span>第{zone}の領域</span>
+        )}
       </h3>
       <ul className='mt-2 list-none'>
         {todos
