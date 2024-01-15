41fd52024ffc5a11624448255f79f80b41848fd0
mikurikuri1919 <mikuriyakenta003@gmail.com>
2024-01-15 10:01:19 +0900

    ✨ ドラッグ機能の導入 #127

--- src/features/todo/components/TodoCard.tsx
+++ src/features/todo/components/TodoCard.tsx
@@ -1,3 +1,6 @@
+import { useSortable } from '@dnd-kit/sortable'
+import { CSS } from '@dnd-kit/utilities'
+
 import { Todo } from '../types'
 
 interface Props {
@@ -7,21 +10,65 @@ interface Props {
 export const TodoCard = (props: Props) => {
   const { todo } = props
 
+  const { setNodeRef, attributes, listeners, transform, transition, isDragging } = useSortable({
+    id: todo.id,
+    data: {
+      type: 'Todo',
+      todo,
+    },
+  })
+
+  const style = {
+    transition,
+    transform: CSS.Transform.toString(transform),
+  }
+
+  if (isDragging) {
+    return (
+      <div
+        ref={setNodeRef}
+        style={style}
+        className='
+        hover:ring-2
+        hover:ring-insert
+        hover:ring-gray-500
+        cursor-grab
+        bg-slate-300
+        w-full
+        h-12
+        rounded-md
+        p-2
+        text-center
+        opacity-50
+        '
+      >
+      </div>
+    )
+  }
+
   return (
-    <li
-      className='
-      hover:ring-2
-      hover:ring-insert
-      hover:ring-gray-500
-      cursor-grab
-      bg-slate-300
-      w-48
-      rounded-md
-      p-2
-      text-center
-      '
-    >
-      {todo.title}
-    </li>
+    <div
+      ref={setNodeRef}
+      style={style}
+      {...attributes}
+      {...listeners}
+      className='text-wrap'
+      >
+      <div
+        className='
+        hover:ring-2
+        hover:ring-insert
+        hover:ring-gray-500
+        cursor-grab
+        bg-slate-300
+        w-full
+        rounded-md
+        p-2
+        text-center
+        '
+      >
+        {todo.title}
+      </div>
+    </div>
   )
 }
--- src/features/todo/components/TodoZone.tsx
+++ src/features/todo/components/TodoZone.tsx
@@ -1,40 +1,57 @@
+import { DndContext } from '@dnd-kit/core'
+import type { DragStartEvent } from '@dnd-kit/core'
+import { SortableContext } from '@dnd-kit/sortable'
+import { useMemo } from 'react'
+
 import { Todo } from '../types'
 import { TodoCard } from './TodoCard'
 
 type TodoZoneProps = {
   zone: number
-  todos: Todo[]
+  filterTodos: Todo[]
 }
 
-export const TodoZone = ({ zone, todos }: TodoZoneProps) => (
-  <div
-    className={`overflow-y-auto overflow-x-hidden group relative bg-white p-6 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-500 w-96 h-72 border-l ${
-      zone > 1 ? 'divide-y divide-gray-200' : ''
-    }`}
-  >
-    <div className='mt-1'>
-      <h3 className='text-base font-semibold leading-6 text-gray-900'>
-        {zone === 2 ? (
-          <span className='bg-blue-200 rounded-lg p-1'>第{zone}の領域</span>
-        ) : (
-          <span>第{zone}の領域</span>
-        )}
-      </h3>
-      <ul
-        className='
-        mt-3
-        list-none
-        flex
-        flex-col
-        gap-3
-        '
-      >
-        {todos
-          .filter((todo) => todo.zone === zone)
-          .map((todo) => (
-            <TodoCard key={todo.id} todo={todo} />
-          ))}
-      </ul>
+export const TodoZone = ({ zone, filterTodos }: TodoZoneProps) => {
+  const todosId = useMemo(() => filterTodos.map((todo) => todo.id), [filterTodos])
+
+  const onDragStart = (event: DragStartEvent) => {
+    console.log('event', event)
+  }
+
+  return (
+    <div
+      className={`overflow-y-auto overflow-x-hidden group relative bg-white p-6 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-500 w-96 h-72 border-l ${
+        zone > 1 ? 'divide-y divide-gray-200' : ''
+      }`}
+    >
+      <DndContext onDragStart={onDragStart}>
+        <div className='mt-1'>
+          <h3 className='text-base font-semibold leading-6 text-gray-900'>
+            {zone === 2 ? (
+              <span className='bg-blue-200 rounded-lg p-1'>第{zone}の領域</span>
+            ) : (
+              <span>第{zone}の領域</span>
+            )}
+          </h3>
+          <div
+            className='
+            mt-3
+            list-none
+            flex
+            flex-col
+            gap-3
+            '
+            >
+            <SortableContext items={todosId}>
+              {filterTodos
+                .filter((todo) => todo.zone === zone)
+                .map((todo) => (
+                  <TodoCard key={todo.id} todo={todo} />
+                ))}
+            </SortableContext>
+          </div>
+        </div>
+      </DndContext>
     </div>
-  </div>
-)
+  )
+}
