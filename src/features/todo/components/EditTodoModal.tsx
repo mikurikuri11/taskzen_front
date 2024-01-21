79a6651082975ba2a0e3401c725e65dd57883a4b
mikurikuri1919 <mikuriyakenta003@gmail.com>
2024-01-21 16:25:42 +0900

    🐛 editTodoモーダルの初期表示が変わらない問題に対処 #136

--- src/features/todo/components/EditTodoModal.tsx
+++ src/features/todo/components/EditTodoModal.tsx
@@ -28,6 +28,15 @@ type Props = {
 export const EditTodoModal: FC<Props> = (props) => {
   const { todo, open, setOpen } = props
 
+  useEffect(() => {
+    reset({
+      title: todo?.title,
+      zone: todo?.zone,
+      due_date: todo?.due_date,
+      description: todo?.description,
+    });
+  }, [todo]);
+
   const defaultValues = {
     title: todo?.title,
     zone: todo?.zone,
@@ -244,7 +253,7 @@ export const EditTodoModal: FC<Props> = (props) => {
                       </div>
                       <div className='col-span-full flex gap-4'>
                         <StyledSubmitButton
-                          className='bg-indigo-500 text-lg'
+                          className='bg-indigo-500'
                           disabled={!isDirty || !isValid}
                           // onClick={handleSubmit(onSubmit)}
                         >
