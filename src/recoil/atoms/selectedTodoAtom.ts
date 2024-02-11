e45e0e4af9ab4817fb465d19b8938f0d33415877
mikurikuri1919 <mikuriyakenta003@gmail.com>
2024-02-11 16:28:23 +0900

    🔥 不要なatomの削除 #144

--- src/recoil/atoms/selectedTodoAtom.ts
+++
@@ -1,7 +0,0 @@
-/* eslint-disable */
-import { atom, RecoilState } from 'recoil'
-import { Todo } from '@/features/todo/types'
-
-export const SelectedTodoAtom: RecoilState<Todo | null> = atom({
-  key: 'SelectedTodo',
-})
