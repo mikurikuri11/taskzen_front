0934e959102a7be0968e3a681d08672ee42ceb48
mikurikuri1919 <mikuriyakenta003@gmail.com>
2024-02-12 22:56:03 +0900

    💄 カテゴリー追加ボタンの追加 #163

--- src/features/category/components/category-setting/CategoryTable.tsx
+++ src/features/category/components/category-setting/CategoryTable.tsx
@@ -1,8 +1,9 @@
 'use client'
 
-import { useRouter } from 'next/navigation';
+import { useRouter } from 'next/navigation'
 import { deleteCategory } from '../../api/category/deleteCategory'
 import { Category } from '../../types'
+import { StyledButton } from '@/components/ui-elements/Button/StyledButton'
 import { Id } from '@/features/todo/types'
 
 interface Props {
@@ -11,11 +12,11 @@ interface Props {
 
 export const CategoryTable = (props: Props) => {
   const { categories } = props
-  const router = useRouter();
+  const router = useRouter()
 
-  const handleDelete = async(id: Id) => {
+  const handleDelete = async (id: Id) => {
     await deleteCategory({ id })
-    router.refresh();
+    router.refresh()
   }
 
   return (
@@ -23,7 +24,12 @@ export const CategoryTable = (props: Props) => {
       <div className='px-4 sm:px-6 lg:px-8'>
         <div className='sm:flex sm:items-center'>
           <div className='sm:flex-auto'>
-            <h1 className='text-base font-bold leading-6'>カテゴリー</h1>
+            <div className='flex'>
+              <h1 className='text-2xl font-bold leading-6 mt-4'>カテゴリー</h1>
+              <div className='mt-4 ml-auto sm:mt-0 sm:flex-none'>
+                <StyledButton buttonStyle='bg-indigo-500'>追加する</StyledButton>
+              </div>
+            </div>
             <div className='mt-8 flow-root'>
               <div className='-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8'>
                 <div className='inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8'>
@@ -34,10 +40,11 @@ export const CategoryTable = (props: Props) => {
                           {category.name}
                         </h2>
                         <div className='mt-3 ml-auto'>
-                          <button className='text-indigo-400 hover:text-indigo-300'>
-                            Edit
-                          </button>
-                          <button onClick={() => category.id && handleDelete(category.id)} className='text-red-400 hover:text-red-300 ml-4'>
+                          <button className='text-indigo-400 hover:text-indigo-300'>Edit</button>
+                          <button
+                            onClick={() => category.id && handleDelete(category.id)}
+                            className='text-red-400 hover:text-red-300 ml-4'
+                          >
                             Delete
                           </button>
                         </div>
