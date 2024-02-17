46dd167e82c3ac3b96c23ad09b392de687c9987f
mikurikuri1919 <mikuriyakenta003@gmail.com>
2024-02-17 15:02:15 +0900

    🔥 completedTodoAtom削除 #170

--- src/recoil/atoms/completedTodoAtom.ts
+++
@@ -1,8 +0,0 @@
-/* eslint-disable */
-import { atom, RecoilState } from 'recoil'
-import { Todo } from '@/features/todo/types'
-
-export const completedTodoAtom: RecoilState<Todo[]> = atom<Todo[]>({
-  key: 'completedTodo',
-  default: [],
-})
