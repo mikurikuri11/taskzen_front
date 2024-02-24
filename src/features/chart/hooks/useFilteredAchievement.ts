0fcd966a9f530dc11aec46546eb82a38b69f05bc
mikurikuri1919 <mikuriyakenta003@gmail.com>
2024-02-24 16:28:00 +0900

    🐛 グラフ用データ整形用のカスタムフック修正 #170

--- src/features/chart/hooks/useFilteredAchievement.ts
+++ src/features/chart/hooks/useFilteredAchievement.ts
@@ -4,18 +4,37 @@ import { useAchievement } from '../hooks/useAchievement'
 export const useFilteredAchievements = (userId: string | null) => {
   const { data } = useAchievement(userId)
 
-  const filteredData = useMemo(() => {
-    const oneMonthAgo = new Date()
-    oneMonthAgo.setMonth(oneMonthAgo.getMonth() - 1)
+  const sortedData = useMemo(() => {
+    if (!data) return []
 
-    return data
-      ? data.filter(
-          (item: any) =>
-            new Date(item.achievements_start_date) >= oneMonthAgo &&
-            new Date(item.achievements_end_date) <= new Date(),
-        )
-      : []
+    // データを日付でソート
+    const sortedData = [...data].sort((a, b) => {
+      return (
+        Number(new Date(a.achievements_start_date)) - Number(new Date(b.achievements_start_date))
+      )
+    })
+
+    // 今日の日付を取得
+    const today = new Date()
+
+    // 過去4つのデータを取得
+    const pastFourData = []
+    for (let i = sortedData.length - 1; i >= 0; i--) {
+      const itemDate = new Date(sortedData[i].achievements_start_date)
+      // 今日から過去4つのデータを取得
+      if (itemDate <= today && pastFourData.length < 4) {
+        console.log(sortedData[i])
+        pastFourData.push(sortedData[i])
+      }
+    }
+
+    // pastFourDataが4つ未満の場合は0で埋める
+    while (pastFourData.length < 4) {
+      pastFourData.push({ achievements_start_date: '', achievement_rate: 0 })
+    }
+
+    return pastFourData.reverse()
   }, [data])
 
-  return { filteredData }
+  return { filteredData: sortedData }
 }
