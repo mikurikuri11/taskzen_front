eabf650b28b03ab34a7d2e06f85b8b0bff93b93b
mikurikuri1919 <mikuriyakenta003@gmail.com>
2024-02-28 16:17:15 +0900

    🔥 ToDo作成モーダル用のrecoil削除 #180

--- src/features/todo/components/TodoManagement.tsx
+++ src/features/todo/components/TodoManagement.tsx
@@ -9,7 +9,6 @@ import { StyledButton } from '@/components/ui-elements/Button/StyledButton'
 import { TodoMatrix } from '@/features/todo/components/TodoMatrix'
 import { useSelectTodo } from '@/features/todo/hooks/useSelectTodo'
 import { IncompletedTodoAtom } from '@/recoil/atoms/incompletedTodoAtom'
-import { showCreateTodoModalAtom } from '@/recoil/atoms/showCreateTodoModalAtom'
 import { showEditTodoModalAtom } from '@/recoil/atoms/showEditTodoModalAtom'
 import { Id } from '@/types'
 
@@ -32,14 +31,9 @@ export const TodoManagement: FC = () => {
 
   const { selectedTodo, onSelectTodo } = useSelectTodo()
 
-  const [showCreateTodoModal, setShowCreateTodoModal] = useRecoilState(showCreateTodoModalAtom)
   const [showEditTodoModal, setShowEditTodoModal] = useRecoilState(showEditTodoModalAtom)
 
-  const openModal = async () => {
-    setShowCreateTodoModal(true)
-  }
-
-  const openEditModal = (id?: Id) => {
+  const openModal = (id?: Id) => {
     if (id) {
       onSelectTodo({ id, incompletedTodos, setShowEditTodoModal })
     }
@@ -50,13 +44,13 @@ export const TodoManagement: FC = () => {
       <div className='mx-auto max-w-screen-md flex justify-between my-10'>
         <h1 className='text-white text-2xl font-bold mt-4'>Todo Matrix</h1>
         <div>
-          <StyledButton buttonStyle='bg-indigo-500' onClick={openEditModal}>
+          <StyledButton buttonStyle='bg-indigo-500' onClick={openModal}>
             Todoを作成する
           </StyledButton>
         </div>
       </div>
       <div className='mx-auto max-w-screen-md flex justify-between'>
-        <TodoMatrix todos={incompletedTodos} openEditModal={openEditModal} />
+        <TodoMatrix todos={incompletedTodos} openEditModal={openModal} />
       </div>
       <TodoModal todo={selectedTodo} open={showEditTodoModal} setOpen={setShowEditTodoModal} />
     </div>
