b85ccc4b7f0cc426b49bc06616cd7948dca618cc
mikurikuri1919 <mikuriyakenta003@gmail.com>
2024-02-29 13:40:59 +0900

    🔥 Category関連の不要なコード削除 #180

--- src/features/category/components/category/CategoryFlyoutMenu.tsx
+++ src/features/category/components/category/CategoryFlyoutMenu.tsx
@@ -3,9 +3,8 @@
 import { Popover, Transition } from '@headlessui/react'
 import { PlusIcon } from '@heroicons/react/24/outline'
 import { useSession } from 'next-auth/react'
-import { Fragment, useState, ChangeEvent, Dispatch, SetStateAction } from 'react'
+import { Fragment, Dispatch, SetStateAction } from 'react'
 import { useRecoilState } from 'recoil'
-// import { addCategory } from '../api/category/addCategory'
 import { useCategory } from '../../hooks/useCategory'
 import { CategoryList } from '@/features/category/components/category/CategoryList'
 import { CategoryAtom } from '@/recoil/atoms/categoryAtom'
@@ -24,15 +23,6 @@ export const CategoryFlyoutMenu = (props: CategoryFlyoutMenuProps) => {
   const { data: categoryData } = useCategory(session ? session.user.id : null)
 
   setCategories(categoryData)
-
-  // const onClickAdd = async () => {
-  //   const category = {
-  //     name: inputCategory,
-  //   }
-  //   const newCategory = await addCategory({ category: category, id: session?.user?.id ?? '' })
-  //   setCategories((prevCategories) => [...prevCategories, newCategory])
-  //   setInputCategory('')
-  // }
 
   return (
     <Popover className='relative'>
@@ -58,28 +48,6 @@ export const CategoryFlyoutMenu = (props: CategoryFlyoutMenuProps) => {
               <legend className='sr-only'>Category</legend>
               {/* TODO: CategoryListコンポーネントを表示する新規作成画面で表示されなくなる */}
               <CategoryList todo={todo} setSelectedCategories={setSelectedCategories} />
-              {/* <div className='relative mt-2'>
-                <input
-                  value={inputCategory}
-                  onChange={(e: ChangeEvent<HTMLInputElement>) => setInputCategory(e.target.value)}
-                  type='text'
-                  name='name'
-                  id='name'
-                  className='rounded px-1 peer block w-full border-0 bg-gray-50 py-1.5 text-gray-900 focus:ring-0 sm:text-sm sm:leading-6'
-                  placeholder='Add category'
-                />
-                <div
-                  className='absolute inset-x-0 bottom-0 border-t border-gray-300 peer-focus:border-t-2 peer-focus:border-indigo-600'
-                  aria-hidden='true'
-                />
-                <button
-                  onClick={onClickAdd}
-                  type='button'
-                  className='mt-1 w-full rounded bg-indigo-50 px-2 py-1 text-xs font-semibold text-indigo-600 shadow-sm hover:bg-indigo-100'
-                >
-                  追加
-                </button>
-              </div> */}
             </fieldset>
           </div>
         </Popover.Panel>
