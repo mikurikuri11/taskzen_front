ed1ce846bb80adafbea8e45c1816f3be63ab057e
mikurikuri1919 <mikuriyakenta003@gmail.com>
2024-03-15 01:04:17 +0900

    💄 レポート周りのUI修正 #196

--- src/features/chart/components/AchievementBoard.tsx
+++ src/features/chart/components/AchievementBoard.tsx
@@ -1,44 +1,40 @@
 import { Flex, Text } from '@mantine/core'
 
 interface Props {
-  weeklyAchievementRate: number | null
+  title: string;
+  achievementRate: number | null;
 }
 
 export const AchievementBoard = (props: Props) => {
-  const { weeklyAchievementRate } = props
+  const { title, achievementRate } = props;
+
+  const getMessage = (rate: number | null) => {
+    if (rate === null) return '';
+    if (rate <= 30) return 'もう少し頑張りましょう。';
+    if (rate <= 55) return 'そこそこできています。もう一踏ん張りです。';
+    if (rate <= 80) return 'とてもいい感じです。これからも頑張りましょう。';
+    return '完璧です。新しいことに挑戦してみましょう。';
+  };
 
   return (
-    <>
-      <Flex
-        className='overflow-hidden rounded-xl border border-gray-200 bg-gray-50 p-6'
-        mih={50}
-        gap='md'
-        justify='center'
-        align='center'
-        direction='column'
-        wrap='wrap'
-      >
-        <Text size='lg' fw={700}>
-          今週の達成率
-        </Text>
-        <Text size='xl' fw={700}>
-          {weeklyAchievementRate} %
-        </Text>
-        <Text size='sm' c='dimmed'>
-          {weeklyAchievementRate !== null && (
-            <>
-              {weeklyAchievementRate <= 30 && 'もう少し頑張りましょう。'}
-              {weeklyAchievementRate > 30 &&
-                weeklyAchievementRate <= 55 &&
-                'そこそこできています。もう一踏ん張りです。'}
-              {weeklyAchievementRate > 55 &&
-                weeklyAchievementRate <= 80 &&
-                'とてもいい感じです。これからも頑張りましょう。'}
-              {weeklyAchievementRate > 80 && '完璧です。新しいことに挑戦してみましょう。'}
-            </>
-          )}
-        </Text>
-      </Flex>
-    </>
-  )
-}
+    <Flex
+      className={`overflow-hidden rounded-xl border border-gray-200 bg-gray-100 p-6`}
+      mih={50}
+      gap='md'
+      justify='center'
+      align='center'
+      direction='column'
+      wrap='wrap'
+    >
+      <Text size='lg' fw={700}>
+        {title}
+      </Text>
+      <Text size='xl' fw={700}>
+        {achievementRate} %
+      </Text>
+      <Text size='sm' c='dimmed'>
+        {getMessage(achievementRate)}
+      </Text>
+    </Flex>
+  );
+};
