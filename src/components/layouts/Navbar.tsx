cfd520f5af4213b2e13061094d2970e846321793
mikurikuri1919 <mikuriyakenta003@gmail.com>
2024-03-17 12:30:53 +0900

    🔥 不要なコード削除 #198

--- src/components/layouts/Navbar.tsx
+++ src/components/layouts/Navbar.tsx
@@ -3,29 +3,16 @@
 import { Dialog, Transition } from '@headlessui/react'
 import {
   Bars3Icon,
-  CalendarIcon,
-  ChartPieIcon,
-  DocumentDuplicateIcon,
-  FolderIcon,
-  HomeIcon,
-  UsersIcon,
   XMarkIcon,
 } from '@heroicons/react/24/outline'
 
-import { Center, Tooltip, UnstyledButton, Stack, rem } from '@mantine/core'
+import { Tooltip, Stack, rem } from '@mantine/core'
 import { useDisclosure } from '@mantine/hooks'
 import { modals } from '@mantine/modals'
 import {
   IconHome2,
-  IconGauge,
-  IconDeviceDesktopAnalytics,
-  IconFingerprint,
-  IconCalendarStats,
-  IconUser,
-  IconSettings,
   IconLogout,
   IconCheck,
-  IconSwitchHorizontal,
   IconCategory2,
   IconChartLine,
   IconInfoSquareRounded,
@@ -164,11 +151,6 @@ export const Navbar = () => {
 
                   <div className='flex grow flex-col gap-y-5 overflow-y-auto bg-gray-800 px-6 pb-2 ring-1 ring-white/10'>
                     <div className='flex h-16 shrink-0 items-center'>
-                      {/* <img
-                        className='h-8 w-auto'
-                        src='https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500'
-                        alt='Your Company'
-                      /> */}
                       <Image
                         src='/taskzen-logo.png'
                         alt=''
@@ -226,14 +208,6 @@ export const Navbar = () => {
             <Bars3Icon className='h-6 w-6' aria-hidden='true' />
           </button>
           <div className='flex-1 text-sm font-semibold leading-6 text-white'>Dashboard</div>
-          {/* <a href='#'>
-            <span className='sr-only'>Your profile</span>
-            <img
-              className='h-8 w-8 rounded-full bg-gray-800'
-              src='https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80'
-              alt=''
-            />
-          </a> */}
           <FlyoutMenu />
         </div>
       </div>
